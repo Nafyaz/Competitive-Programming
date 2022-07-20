1fdcb907b7d9078dc2ad58520724cc8daa79e8c1
unknown <nafis.faiyaz.5@gmail.com>
2022-07-20 19:32:16 +0600

    regular update

--- 1 Code Library/1 Templates/Nafis Template.cpp
+++ 1 Code Library/1 Templates/Nafis Template.cpp
@@ -19,6 +19,7 @@ using namespace std;
 #define ll long long
 #define show(x) cout << #x << ": " << x << "; "
 #define pll pair<ll, ll>
+#define pii pair<int, int>
 #define ff first
 #define ss second
 #define MOD 1000000007
@@ -38,8 +39,8 @@ int main()
 
     cin >> T;
 
-    while(T--);
+    while(T--)
     {
-        solve(caseno);
+        solve(++caseno);
     }
 }
